95fc16ceb6ecf5cd87e86afd8cc4392f10bb3a9e
jalguacilgon445 <jesus.alguacilgon@gmail.com>
2025-02-24 16:11:09 +0100

    Added security group to 002_learn

--- 002_learn-terraform-aws-instance/main.tf
+++ 002_learn-terraform-aws-instance/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 2.0"
+    }
   }
 
   required_version = ">= 1.2.0"
@@ -13,9 +17,40 @@ provider "aws" {
   region  = "eu-north-1"
 }
 
+data "http" "my_ip" {
+  url = "https://api.ipify.org"
+}
+
+# Create a security group to allow traffic to the resources and between them
+resource "aws_security_group" "ec2_sg" {
+
+  ingress { # Allow SSH inbound traffic from developer's IP
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${data.http.my_ip.response_body}/32"]
+  }
+
+  ingress { # Allow all HTTP inbound traffic (for WebServer access)
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {  # Allow all outbound traffic
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-031f286cefaaf26c2"
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name = "ssh-ec2-keypair"
 
   tags = {
     Name = "MyWebServerFromTerraform"
